8564f379f9475de52d3d31073b6435ca4eb5772e
nicholas landreville <nl@nicholass-MacBook-Air.local>
2020-04-19 12:34:30 -0500

    Updates

---
+++ precache-manifest.fde2d7901b0c622bcd554e6c1527fbd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e315e1082f4473863426df4dcdad0be1",
+    "url": "/typescript-todo/index.html"
+  },
+  {
+    "revision": "e67c41f31627fc009f23",
+    "url": "/typescript-todo/static/css/main.576a186a.chunk.css"
+  },
+  {
+    "revision": "d35d2122707879634f03",
+    "url": "/typescript-todo/static/js/2.72c1801b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typescript-todo/static/js/2.72c1801b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67c41f31627fc009f23",
+    "url": "/typescript-todo/static/js/main.668d09c7.chunk.js"
+  },
+  {
+    "revision": "d3d3d6a0573f4d37f5d0",
+    "url": "/typescript-todo/static/js/runtime-main.a5fc79a5.js"
+  }
+]);
